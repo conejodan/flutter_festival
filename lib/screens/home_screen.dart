dca768139470ce993a828eced0a8b3841eeebc0b
Daniel Sanchez <conejodan@gmail.com>
2022-03-20 09:10:28 -0600

    Creando ejemplo de Widgets Basicos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,8 +11,79 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: const Center(
-        child: Text('MyHomePag'),
+      backgroundColor: Colors.grey.shade300,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                width: 300,
+                child: Column(
+                  children: [
+                    Image(
+                      image: NetworkImage('https://picsum.photos/800/400'),
+                      fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Bonita lugar a visitar',
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                              "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. "),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              FlutterLogo(),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Icon(
+                                Icons.share,
+                                color: Colors.green,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          ElevatedButton(
+                              onPressed: () => {}, child: Text('Visitar')),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
